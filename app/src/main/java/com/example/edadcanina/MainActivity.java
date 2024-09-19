8fb6d89a7808931e531fe5d1c62a9cbb02d456d6
Adrian Bañuelas <adriangpe5666@gmail.com>
2024-09-18 20:12:14 -0600

    Se realiza el cálculo de la edad del can

--- app/src/main/java/com/example/edadcanina/MainActivity.java
+++ app/src/main/java/com/example/edadcanina/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.edadcanina;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +14,28 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText age;
+    private TextView textResult;
+    private Button btnCalculate;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        age = findViewById(R.id.age);
+        textResult = findViewById(R.id.text_result);
+        btnCalculate = findViewById(R.id.btnCalcular);
+
+        btnCalculate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int dogAge = Integer.parseInt(age.getText().toString());
+                int result = dogAge * 7;
+                String resultStr = "La edad del can es " + result + " años.";
+                textResult.setText(resultStr);
+            }
         });
     }
 }
